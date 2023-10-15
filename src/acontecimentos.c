a0c1ff2dad93308fb58788cb287410d6545d1110
Rúben Campos <afonsocampo06@gmail.com>
2023-10-15 16:49:18 +0100

    added files

--- src/acontecimentos.c
+++ src/acontecimentos.c
@@ -1,6 +1,12 @@
 #include "acontecimentos.h"
 
 //o funcionamento da função seria "idUtilizador hora "+mensagemAcontecimento() 
+/**
+ * This function returns a message based on the given event.
+ * 
+ * @param acontecimento The parameter "acontecimento" is an integer that represents a specific event or
+ * occurrence.
+ */
 char* mensagemAcontecimento(int acontecimento){
     switch(acontecimento){
         case(SimulationStart):{
