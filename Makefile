481350ff2eb52d1aa6a9adadc171b4447bd06d78
jpedro85 <pedro.games30@gmail.com>
2023-10-17 11:18:04 +0000

    patch:  - fixed Makefile error where there were spaces instead of tabulations feat:  - implemented loading json content inside .conf file

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ SRCDIR = ./src
 BUILD_DIR = ./build
 
 # Add flags if needed
-CFLAGS = -lpthread
+CFLAGS = -lpthread -lcjson
 
 # Add common files if needed
-COMMONFILES = 
+COMMONFILES = $(SRCDIR)/file.c
 
 # List of source files for monitor and simulador
 MONITOR_SRC = $(SRCDIR)/monitor/*.c
@@ -21,22 +21,21 @@ SIMULADOR_OBJ = $(BUILD_DIR)/simulador
 default: build 
 
 init:
-    mkdir -p $(BUILD_DIR) $(SRCDIR)
-    touch $(SRCDIR)/main.c
+	mkdir -p $(BUILD_DIR) $(SRCDIR)
 
 build: monitor simulador
 
 monitor:
-    $(CC) -g $(MONITOR_SRC) $(COMMONFILES) $(CFLAGS) -o $(MONITOR_OBJ)
+	$(CC) -g $(MONITOR_SRC) $(COMMONFILES) $(CFLAGS) -o $(MONITOR_OBJ)
 
 simulador:
-    $(CC) -g $(SIMULADOR_SRC) $(COMMONFILES) $(CFLAGS) -o $(MONITOR_OBJ)
+	$(CC) -g $(SIMULADOR_SRC) $(COMMONFILES) $(CFLAGS) -o $(SIMULADOR_OBJ)
 
 run-monitor:
-    $(MONITOR_OBJ)
+	$(MONITOR_OBJ)
 
 run-simulador:
-    $(SIMULADOR_OBJ)
+	$(SIMULADOR_OBJ)
 
 clean:
-    rm -f $(BUILD_DIR)/*
+	rm -f $(BUILD_DIR)/*
---
+++ src/file.c
@@ -0,0 +1,93 @@
+#include "file.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *readJSON(char *fileName);
+
+cJSON *loadJSON(char *fileName)
+{
+    char *jsonContent = readJSON(fileName);
+
+    cJSON *loadedContent = cJSON_Parse(jsonContent);
+    free(jsonContent);
+
+    if (loadedContent == NULL)
+    {
+        const char *errorPtr = cJSON_GetErrorPtr();
+        if (errorPtr != NULL)
+        {
+            printf("Loading Error: %s\n", errorPtr);
+        }
+        cJSON_Delete(loadedContent);
+        return NULL;
+    }
+    return loadedContent;
+}
+
+/**
+ * The function `readJSON` reads the contents of a JSON file and returns it as a character array.
+ *
+ * @param fileName The fileName parameter is a string that represents the name of the JSON file that
+ * you want to read.
+ *
+ * @return The function `readJSON` returns a pointer to a character array (`char *`).
+ */
+char *readJSON(char *fileName)
+{
+    FILE *fileReader = fopen(fileName, "r");
+
+    if (fileReader == NULL)
+    {
+        perror("Error: Unable to open the file.\n");
+        return NULL;
+    }
+
+    // Calculates the file size
+    fseek(fileReader, 0, SEEK_END);
+    size_t fileSize = ftell(fileReader);
+    fseek(fileReader, 0, SEEK_SET);
+
+    if (fileSize == -1)
+    {
+        perror("Failed to determine file size");
+        fclose(fileReader);
+        return NULL;
+    }
+
+    // Allocates buffer size to same size as the file
+    char *buffer = (char *)malloc(fileSize);
+    if (buffer == NULL)
+    {
+        perror("Failed to allocate memory");
+        fclose(fileReader);
+        return NULL;
+    }
+
+    size_t contentRead = fread(buffer, 1, fileSize, fileReader);
+    fclose(fileReader);
+
+    if (contentRead != fileSize)
+    {
+        perror("Failed to read file");
+        fclose(fileReader);
+        free(buffer);
+        return NULL;
+    }
+
+    // cJSON *loadedContent = cJSON_Parse(buffer);
+    // free(buffer);
+
+    // if (loadedContent == NULL)
+    // {
+    //     const char *errorPtr = cJSON_GetErrorPtr();
+    //     if (errorPtr != NULL)
+    //     {
+    //         printf("Loading Error: %s\n", errorPtr);
+    //     }
+    //     cJSON_Delete(loadedContent);
+    //     return NULL;
+    // }
+
+    return buffer;
+}
